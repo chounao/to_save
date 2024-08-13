4437ec32b69ec358d78a27d01ad07313a3360bd9
chaojiezhou <85343028+chounao@users.noreply.github.com>
2024-08-13 17:22:37 +0800

    Add files via upload

--- world_to_excel/work_tools.py
+++ world_to_excel/work_tools.py
@@ -1,24 +1,27 @@
-from docx import Document
 import os
-import pandas as pd
 import re
-class to_Excel:
+import pandas as pd
+from docx import Document
+
+
+class To_excel:
 	def __init__(self):
 		self.tables_data_first_four = []  # 用于存放前四行的数据
 		self.tables_data_fifth_row = []  # 用于存放第五行的数据
 		self.tables_data_sixth_row = []  # 用于存放第五行的数据
 		self.big_dicts = {}
 		self.phone = None
-		self.hometonw = None
-		self.addres = None
+		self.native_place = None
+		self.address = None
 		self.path_files = None
-	
+		self.body = None
+		self.dit_lis = []
 	def read_world(self, file_path):
-		'''
+		"""
 		主要是读取world文档的
 		:param file_path: 文件路径
 		:return:
-		'''
+		"""
 		doc = Document(file_path)
 		for table in doc.tables:
 			table_data_first_four = []
@@ -43,11 +46,11 @@ class to_Excel:
 				self.tables_data_sixth_row.append(table_data_sixfh_row)
 	
 	def save_dict(self, file_path):
-		'''
+		"""
 		主要是拼装内容的
 		:param file_path: world文档路径，
 		:return: self.body 是每次拼装成的字典
-		'''
+		"""
 		self.read_world(file_path)
 		# 处理前四行
 		list1 = []
@@ -72,56 +75,54 @@ class to_Excel:
 					# pass
 					self.phone = i
 				if '地址' in i:
-					self.hometonw = i.split("：")[1]
+					self.native_place = i.split("：")[1]
 		# 处理第六行
 		for table_data in self.tables_data_sixth_row:
 			for i in set(table_data):
 				if "@" in i:
 					email_data = i
 				if '地址' in i:
-					self.addres = i.split("：")[1]
-		#处理下银行卡区分银行卡和开户行
+					self.address = i.split("：")[1]
+		# 处理下银行卡区分银行卡和开户行
 		bank = self.big_dicts.get('中国银行\n卡号及开户行')
-		bank_addres = re.findall(r'[\u4e00-\u9fa5]',bank) #获取汉字
-		str_bank_addres = re.sub(r'\s','',(' '.join(bank_addres)))#获取的汉字是list转str.并去掉转后的空格
-		bank_id =re.findall(r'\d+',bank)
+		bank_address = re.findall(r'[\u4e00-\u9fa5]', bank)  # 获取汉字
+		str_bank_address = re.sub(r'\s', '', (' '.join(bank_address)))  # 获取的汉字是list转str.并去掉转后的空格
+		bank_id = re.findall(r'\d+', bank)
 		str_bank_id = ' '.join(bank_id)
 		self.body = {
 			'员工姓名': self.big_dicts.get("姓名"),
 			'学校': self.big_dicts.get("毕业院校"),
 			'专业': self.big_dicts.get("专业"),
 			'婚姻状况': self.big_dicts.get("婚姻状况"),
-			'住址': self.addres,
+			'住址': self.address,
 			'党员身份': self.big_dicts.get("党团员"),
 			'户籍所在地': self.big_dicts.get("籍贯\n（省/市/县）"),
 			'身份证号': self.big_dicts.get('身份证号码'),
 			'工资卡号': str_bank_id,
-			'开户行':str_bank_addres,
-			'手机号码': self.phone }
-		# body['address'] = big_dicts.get("现通讯地址")
-		# self.body['Registered'] = self.hometonw
-		# print(self.body)
-		
+			'开户行': str_bank_address,
+			'手机号码': self.phone
+		}
+
 		return self.body
 	
-	def get_all_path(self,s):
-		#当前目录的文件 s为格式内容
-		'''
+	def get_all_path(self, s):
+		# 当前目录的文件 s为格式内容
+		"""
 		:param s: 格式的内容 s可以填上'.docx'/'.xlsx'
 		:return: 返回所有文件路径
-		'''
+		"""
 		current_path = os.getcwd()
 		files_and_dirs = os.listdir(current_path)
 		self.path_files = [os.path.join(current_path, file) for file in files_and_dirs if file.endswith(s)]
-
+		
 		return self.path_files
 	
 	def all_world_to_dict(self):
-		'''
+		"""
 		整理body的数据把这些数据变成需要存储成excel需要的类型
 		:return:
-		'''
-		self.dit_lis = []
+		"""
+		
 		self.get_all_path('.docx')
 		for i in self.path_files:
 			body = self.save_dict(i)
@@ -138,10 +139,10 @@ class to_Excel:
 		return dic
 	
 	def save_excel(self):
-		'''
+		"""
 		把整理的数据存到Excel表里
 		:return:
-		'''
+		"""
 		xlsx_path = self.get_all_path('.xlsx')[0]
 		data = self.all_world_to_dict()
 		pf = pd.DataFrame(data)
@@ -150,5 +151,5 @@ class to_Excel:
 
 
 if __name__ == '__main__':
-	a = to_Excel()
+	a = To_excel()
 	a.save_excel()
